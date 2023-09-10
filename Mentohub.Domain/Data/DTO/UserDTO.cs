29727387c78739431d85f2b1c54b3ff7ca7b579e
Alla Ponurko <aponurko@gmail.com>
2023-09-10 19:37:50 +0200

    userDTO test

---
+++ Mentohub.Domain/Data/DTO/UserDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mentohub.Domain.Data.DTO
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+
+    }
+}
