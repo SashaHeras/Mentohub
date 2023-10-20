93bc0dfbf433757827e2f196f47db264c81e7a35
AllaPonurko <96493346+AllaPonurko@users.noreply.github.com>
2023-10-21 00:23:41 +0200

    Add files via upload

---
+++ Mentohub.Domain/Data/Entities/CurrentUser.cs
@@ -0,0 +1,14 @@
+﻿using Mentohub.Domain.Data.Entities.Interfaces;
+using Microsoft.AspNetCore.Identity;
+
+namespace Mentohub.Domain.Data.Entities
+{
+    public class CurrentUser: IdentityUser, IItem
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Image { get; set; }
+        public string? AboutMe { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
