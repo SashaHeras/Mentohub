1ada1923b38a7d64386948e8dd8efd8e482bc3a9
AllaPonurko <96493346+AllaPonurko@users.noreply.github.com>
2023-10-21 00:22:15 +0200

    Add files via upload

---
+++ Mentohub.Core/AllExceptions/AllException.cs
@@ -0,0 +1,44 @@
+﻿using Mentohub.Domain.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mentohub.Core.AllExceptions
+{
+    public class AllException:Exception
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public CurrentUser NotFoundObjectResult(string v)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public bool RankException(string v)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public string ArgumentNullException(string v)
+        {
+            throw new NotImplementedException();
+        }
+        public void NewException(string v) { }
+    }
+}
