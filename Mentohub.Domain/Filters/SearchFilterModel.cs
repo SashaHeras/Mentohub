fad4f5d1729c680be8b79d642fffd9840c8111ef
Саша Герас <acseler16@gmail.com>
2023-12-31 14:59:49 +0200

    Додано фільтрий пошуку

---
+++ Mentohub.Domain/Filters/SearchFilterModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mentohub.Domain.Filters
+{
+    public class SearchFilterModel
+    {
+        public List<int> Categories { get; set; } = new List<int>();
+
+        public int LanguageID { get; set; }
+
+        public double Rate { get; set; }
+
+        public int PriceFrom { get; set; }
+
+        public int PriceTo { get; set; }
+    }
+}
