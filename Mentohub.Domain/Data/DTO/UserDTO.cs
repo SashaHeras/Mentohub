e23b4957eff4fc3e76eadb430be78e315961aea6
Саша Герас <acseler16@gmail.com>
2024-02-05 20:17:15 +0200

    Додавання номеру телефону до даних куростувача

--- Mentohub.Domain/Data/DTO/UserDTO.cs
+++ Mentohub.Domain/Data/DTO/UserDTO.cs
@@ -14,9 +14,11 @@ namespace Mentohub.Domain.Data.DTO
         
         public string? FirstName { get; set; }
         
-        public string? LastName { get; set; }
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
         
-        public string? AboutMe { get; set; }
+        public string AboutMe { get; set; }
 
         public string? EncryptedID { get; set; }
 
